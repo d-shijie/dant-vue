2b5a78d134ed31175af42eb6f30eff00dbcc80ea
dshijie <943996999@qq.com>
2022-12-12 17:48:50 +0800

    feat: add slider component

--- src/dant/components/slider/DSlider.vue
+++ src/dant/components/slider/DSlider.vue
@@ -1,23 +1,58 @@
 <template>
-  <div class="d-slider" />
+  <div class="d-slider" @click="handleClick">
+    <span ref="dantSliderTagRef" class="d-slider__tag" @mousedown="handleMouseDown" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 defineProps({
   modelValue: {
     type: [String, Number],
     default: 0
   }
 })
+const dantSliderTagRef = ref()
+const handleMouseDown = () => {
+  console.log(dantSliderTagRef.value.style.left)
+}
+const handleMouseMove = (e:any) => {
+  dantSliderTagRef.value.style.left = e.offsetX + 'px'
+}
+const handleClick = (e:any) => {
+  dantSliderTagRef.value.style.left = e.offsetX + 'px'
+}
+
 </script>
 
 <style scoped lang="scss">
- .d-slider {
-  width: 30px;
-  height: 30px;
-  background-color: red;
-  &:active{
-    background-color: blue;
+.d-slider {
+  width: 500px;
+  height: 5px;
+  border-radius: 5px;
+  background-color: #e4e7ed;
+  position: relative;
+  cursor:pointer;
+  .d-slider__tag {
+    display: inline-block;
+    width: 15px;
+    position: absolute;
+    height: 15px;
+    border-radius: 50%;
+    left: 0;
+    top: 2.5px;
+    transform: translateY(-50%);
+    border: 2px solid blue;
+    background-color: #fff;
+
+    &:active {
+      background-color: blue;
+
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
   }
- }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-
-})
+// const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+module.exports = {
+  transpileDependencies: true,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, './src/dant/style/global.scss')
+      ]
+    }
+  }
+}
